3d7449a0adae7feac8ab1a80c6907659b488b65c
Philipp Schulz <hadeweka@outlook.de>
2020-03-09 15:24:07 +0100

    Reorganized directories

--- Main.cpp
+++ Main.cpp
@@ -62,18 +62,18 @@ int main(int argc, char** argv) {
 
 	//! Load Ruby core scripts
 
-	MRB_LOAD_CORE_SCRIPT_FOLDER(mrb, include, scripts_include);
-	MRB_LOAD_CORE_SCRIPT_FOLDER(mrb, core, scripts_core);
+	MRB_LOAD_CORE_SCRIPT_FOLDER(mrb, include, kernel_scripts/include);
+	MRB_LOAD_CORE_SCRIPT_FOLDER(mrb, core, kernel_scripts/core);
 
 	//! Load main scripts
 	//! If you want to add something, put it here
 	//! These are ordered, so don't just put it anywhere if it depends on other scripts
 	//! If you want to add compiled Ruby scripts, you also need to include them in the header file
 
-	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_resources, scripts_custom/resources);
-	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_scenes, scripts_custom/scenes);
-	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_entities, scripts_custom/entities);
-	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_other, scripts_custom/other);
+	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_resources, kernel_scripts/custom/resources);
+	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_scenes, kernel_scripts/custom/scenes);
+	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_entities, kernel_scripts/custom/entities);
+	MRB_LOAD_SCRIPT_FOLDER(mrb, custom_other, kernel_scripts/custom/other);
 
 #ifdef SHIDACEA_MOD_LOADING
 
@@ -85,11 +85,11 @@ int main(int argc, char** argv) {
 
 	//! Load game class
 
-	MRB_LOAD_SCRIPT(mrb, custom_game, scripts_custom/Game);
+	MRB_LOAD_SCRIPT(mrb, custom_game, kernel_scripts/custom/Game);
 
 	//! Start main script with the game loop
 
-	MRB_LOAD_SCRIPT(mrb, custom_main, scripts_custom/Main);
+	MRB_LOAD_SCRIPT(mrb, custom_main, kernel_scripts/custom/Main);
 
 	//! Clean up the Ruby interpreter
 
