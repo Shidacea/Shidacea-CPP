54e9c58adf7fda42b650224c527fd63648c0d6e4
Philipp Schulz <hadeweka@outlook.de>
2020-04-05 14:55:32 +0200

    Removed border case distinction for collisions

--- core/internal/Collisions.h
+++ core/internal/Collisions.h
@@ -5,16 +5,7 @@
 //! Here resides the actual mathematical core of the collision routines
 //! These are completely decoupled from any Ruby or SFML magic
 
-//! The following defines allow for simple changes of the collision routines
-//! This is especially useful if you want to include border cases
-//! The algorithms are usually checking non-collision-criteria, so an <= means rejection at direct contact
-//! Reasonable combinations are <=,>=,<,>,false and <,>,<=,>=,true
-
-#define LESS_THAN <=
-#define GREATER_THAN >=
-#define LESS_THAN_ACCEPT <
-#define GREATER_THAN_ACCEPT >
-#define EQUAL_OVERLAP false
+//! Sadly, std::abs is not a constexpr, so this definition will provide one
 
 template <class T> constexpr T constexpr_abs(T value) {
 
@@ -26,17 +17,13 @@ constexpr bool collision_point_point(float x1, float y1, float x2, float y2) {
 
 	//! Usually, this routine will yield false
 
-	if (EQUAL_OVERLAP) return (x1 == x2 && y1 == y2);
+	return (x1 == x2 && y1 == y2);
 
 	return false;
 
 }
 
 constexpr bool collision_point_line(float x1, float y1, float x2, float y2, float dx2, float dy2) {
-
-	//! If equal overlap is not set, this is always false
-
-	if (!EQUAL_OVERLAP) return false;
 
 	//! First, check whether the point is behind one of the line segment points
 	//! It is not necessary to check whether the point is actually on the line yet
@@ -45,12 +32,12 @@ constexpr bool collision_point_line(float x1, float y1, float x2, float y2, floa
 
 	auto dx12 = x1 - x2;
 
-	if (static_cast<bool>(dx12 LESS_THAN 0.0) != static_cast<bool>(dy2 LESS_THAN 0.0)) return false;
+	if (static_cast<bool>(dx12 < 0.0) != static_cast<bool>(dy2 < 0.0)) return false;
 
 	//! The next check tests whether the line parameter is greater than 1
 	//! Checking the absolute values is sufficient, as the signs of dx12 and dy2 is now proven equal
 
-	if (constexpr_abs(dx12) LESS_THAN constexpr_abs(dy2)) return false;
+	if (constexpr_abs(dx12) < constexpr_abs(dy2)) return false;
 
 	auto dy12 = y1 - y2;
 
@@ -73,7 +60,7 @@ constexpr bool collision_point_circle(float x1, float y1, float x2, float y2, fl
 	auto dx = x1 - x2;
 	auto dy = y1 - y2;
 
-	if (dx * dx + dy * dy GREATER_THAN r2 * r2) return false;
+	if (dx * dx + dy * dy > r2 * r2) return false;
 	
 	return true;
 
@@ -83,10 +70,10 @@ constexpr bool collision_point_box(float x1, float y1, float x2, float y2, float
 
 	//! Literally the definition of an AABB
 
-	if (x1 LESS_THAN x2 - w2) return false;
-	if (y1 LESS_THAN y2 - h2) return false;
-	if (x2 + w2 LESS_THAN x1) return false;
-	if (y2 + h2 LESS_THAN y1) return false;
+	if (x1 < x2 - w2) return false;
+	if (y1 < y2 - h2) return false;
+	if (x2 + w2 < x1) return false;
+	if (y2 + h2 < y1) return false;
 	
 	return true;
 }
@@ -116,7 +103,7 @@ constexpr bool collision_line_line(float x1, float y1, float dx1, float dy1, flo
 
 	//! If nominator and denominator signs are different, the parameter is smaller than 0
 
-	if (static_cast<bool>(nominator_1 LESS_THAN 0.0) != static_cast<bool>(denominator_1 LESS_THAN 0.0)) return false;
+	if (static_cast<bool>(nominator_1 < 0.0) != static_cast<bool>(denominator_1 < 0.0)) return false;
 
 	//! First line parameter is positive, so check the other one
 
@@ -126,14 +113,14 @@ constexpr bool collision_line_line(float x1, float y1, float dx1, float dy1, flo
 	auto nominator_2 = dx21_y1 - x1_dy21;
 	auto denominator_2 = dx1_dy2 - dx2_dy1;
 
-	if (static_cast<bool>(nominator_2 LESS_THAN 0.0) != static_cast<bool>(denominator_2 LESS_THAN 0.0)) return false;
+	if (static_cast<bool>(nominator_2 < 0.0) != static_cast<bool>(denominator_2 < 0.0)) return false;
 
 	//! Check whether one of the line parameters is greater than 1
 	//! This can again be done by comparing nominator and denominator
 	//! Since the case of a negative parameter is already excluded, the absolute values are sufficient here
 
-	if (constexpr_abs(denominator_1) LESS_THAN constexpr_abs(nominator_1)) return false;
-	if (constexpr_abs(denominator_2) LESS_THAN constexpr_abs(nominator_2)) return false;
+	if (constexpr_abs(denominator_1) < constexpr_abs(nominator_1)) return false;
+	if (constexpr_abs(denominator_2) < constexpr_abs(nominator_2)) return false;
 
 	//! Both line parameters are between 0 and 1, so a collision is definitely happening
 
@@ -171,7 +158,7 @@ constexpr bool collision_line_box(float x1, float y1, float dx1, float dy1, floa
 	//! Check whether the y coordinate of the intersection point with the left AABB side is actually inside the AABB
 	//! The following checks will repeat this procedure for the other sides
 
-	if ((nom_x_neg_dy GREATER_THAN_ACCEPT nom_y_neg_dx) && (nom_x_neg_dy LESS_THAN_ACCEPT nom_y_pos_dx)) {
+	if ((nom_x_neg_dy >= nom_y_neg_dx) && (nom_x_neg_dy <= nom_y_pos_dx)) {
 
 		//! The case of a vanishing dx1 should not occur, but even then, the next check will rule it out definitely
 		//! Here, the line parameter of the intersection point will be checked for its sign
@@ -179,12 +166,12 @@ constexpr bool collision_line_box(float x1, float y1, float dx1, float dy1, floa
 		//! This can again be checked by comparing the signs of the nominator and the denominator
 		//! We still need to check the other sides, however
 
-		if (static_cast<bool>(nominator_x_neg LESS_THAN 0.0) == static_cast<bool>(dx1 LESS_THAN 0.0)) {
+		if (static_cast<bool>(nominator_x_neg < 0.0) == static_cast<bool>(dx1 < 0.0)) {
 
 			//! Check whether the line parameter is smaller than 1
 			//! If it is, a collision definitely occurs here and we can finally return a value
 
-			if (constexpr_abs(nominator_x_neg) LESS_THAN_ACCEPT constexpr_abs(dx1)) return true;
+			if (constexpr_abs(nominator_x_neg) <= constexpr_abs(dx1)) return true;
 
 		}
 
@@ -192,13 +179,13 @@ constexpr bool collision_line_box(float x1, float y1, float dx1, float dy1, floa
 
 	//! Check right side
 
-	if ((nom_x_pos_dy GREATER_THAN_ACCEPT nom_y_neg_dx) && (nom_x_pos_dy LESS_THAN_ACCEPT nom_y_pos_dx)) {
+	if ((nom_x_pos_dy >= nom_y_neg_dx) && (nom_x_pos_dy <= nom_y_pos_dx)) {
 
 		//! The line got shifted in its coordinates, so a new line parameter check is necessary
 
-		if (static_cast<bool>(nominator_x_pos LESS_THAN 0.0) == static_cast<bool>(dx1 LESS_THAN 0.0)) {
+		if (static_cast<bool>(nominator_x_pos < 0.0) == static_cast<bool>(dx1 < 0.0)) {
 
-			if (constexpr_abs(nominator_x_pos) LESS_THAN_ACCEPT constexpr_abs(dx1)) return true;
+			if (constexpr_abs(nominator_x_pos) <= constexpr_abs(dx1)) return true;
 
 		}
 
@@ -206,11 +193,11 @@ constexpr bool collision_line_box(float x1, float y1, float dx1, float dy1, floa
 
 	//! Check bottom side
 
-	if ((nom_y_neg_dx GREATER_THAN_ACCEPT nom_x_neg_dy) && (nom_y_neg_dx LESS_THAN_ACCEPT nom_x_pos_dy)) {
+	if ((nom_y_neg_dx >= nom_x_neg_dy) && (nom_y_neg_dx <= nom_x_pos_dy)) {
 
-		if (static_cast<bool>(nominator_y_neg LESS_THAN 0.0) == static_cast<bool>(dy1 LESS_THAN 0.0)) {
+		if (static_cast<bool>(nominator_y_neg < 0.0) == static_cast<bool>(dy1 < 0.0)) {
 
-			if (constexpr_abs(nominator_y_neg) LESS_THAN_ACCEPT constexpr_abs(dy1)) return true;
+			if (constexpr_abs(nominator_y_neg) <= constexpr_abs(dy1)) return true;
 
 		}
 
@@ -218,11 +205,11 @@ constexpr bool collision_line_box(float x1, float y1, float dx1, float dy1, floa
 
 	//! Check top side
 
-	if ((nom_y_pos_dx GREATER_THAN_ACCEPT nom_x_neg_dy) && (nom_y_pos_dx LESS_THAN_ACCEPT nom_x_pos_dy)) {
+	if ((nom_y_pos_dx >= nom_x_neg_dy) && (nom_y_pos_dx <= nom_x_pos_dy)) {
 
-		if (static_cast<bool>(nominator_y_pos LESS_THAN 0.0) == static_cast<bool>(dy1 LESS_THAN 0.0)) {
+		if (static_cast<bool>(nominator_y_pos < 0.0) == static_cast<bool>(dy1 < 0.0)) {
 
-			if (constexpr_abs(nominator_y_pos) LESS_THAN_ACCEPT constexpr_abs(dy1)) return true;
+			if (constexpr_abs(nominator_y_pos) <= constexpr_abs(dy1)) return true;
 
 		}
 
@@ -241,7 +228,7 @@ constexpr bool collision_circle_circle(float x1, float y1, float r1, float x2, f
 
 	auto combined_radius = r1 + r2;
 
-	if (dx * dx + dy * dy GREATER_THAN combined_radius * combined_radius) return false;
+	if (dx * dx + dy * dy > combined_radius * combined_radius) return false;
 
 	return true;
 
@@ -251,10 +238,10 @@ constexpr bool collision_box_box(float x1, float y1, float w1, float h1, float x
 
 	//! Simple generalization of point/box
 
-	if (x1 + w1 LESS_THAN x2 - w2) return false;
-	if (y1 + h1 LESS_THAN y2 - h2) return false;
-	if (x2 + w2 LESS_THAN x1 - w1) return false;
-	if (y2 + h2 LESS_THAN y1 - h1) return false;
+	if (x1 + w1 < x2 - w2) return false;
+	if (y1 + h1 < y2 - h2) return false;
+	if (x2 + w2 < x1 - w1) return false;
+	if (y2 + h2 < y1 - h1) return false;
 
 	return true;
 
@@ -275,10 +262,10 @@ constexpr bool collision_circle_box(float x1, float y1, float r1, float x2, floa
 
 	//! Check for intersection of the circle projections with the AABB projections
 
-	if (r1 LESS_THAN dxm) return false;
-	if (r1 LESS_THAN dym) return false;
-	if (dxp LESS_THAN - r1) return false;
-	if (dyp LESS_THAN - r1) return false;
+	if (r1 < dxm) return false;
+	if (r1 < dym) return false;
+	if (dxp < - r1) return false;
+	if (dyp < - r1) return false;
 
 	//! Squared values of the points
 
@@ -292,10 +279,10 @@ constexpr bool collision_circle_box(float x1, float y1, float r1, float x2, floa
 	//! Test collision of each border point with the circle
 	//! This is effectively just another point/circle test
 
-	if (r2 GREATER_THAN dxp2 + dyp2) return true;
-	if (r2 GREATER_THAN dxm2 + dyp2) return true;
-	if (r2 GREATER_THAN dxp2 + dym2) return true;
-	if (r2 GREATER_THAN dxm2 + dym2) return true;
+	if (r2 > dxp2 + dyp2) return true;
+	if (r2 > dxm2 + dyp2) return true;
+	if (r2 > dxp2 + dym2) return true;
+	if (r2 > dxm2 + dym2) return true;
 
 	//! The last possibility is that the circle is inside the box, which can be checked quickly
 
@@ -307,7 +294,7 @@ constexpr bool collision_circle_box(float x1, float y1, float r1, float x2, floa
 //! Please submit a bug report if one of these fails
 
 static_assert(false == collision_point_point(1.0f, 2.0f,     3.0f, 4.0f));
-static_assert(EQUAL_OVERLAP == collision_point_point(1.0f, 9.0f,     1.0f, 9.0f));
+static_assert(true == collision_point_point(1.0f, 9.0f,     1.0f, 9.0f));
 
 static_assert(true == collision_point_circle(2.0f, 3.0f,     4.0f, 5.0f, 3.0f));
 
@@ -315,6 +302,7 @@ static_assert(true == collision_point_box(-3.0f, -5.0f,     3.0f, 2.0f, 10.0f, 9
 
 static_assert(true == collision_line_box(3.0f, 2.0f, 8.0f, 11.0f,     5.0f, 6.0f, 5.0f, 5.0f));
 static_assert(false == collision_line_box(11.0f, 0.0f, 11.0f, 13.0f,     5.0f, 6.0f, 5.0f, 5.0f));
+static_assert(true == collision_line_box(1.0f, 1.0f, 7.0f, 7.0f,     4.0f, 4.0f, 2.0f, 2.0f));
 
 static_assert(true == collision_box_box(1.0f, 2.0f, 3.0f, 4.0f,     4.5f, 7.5f, 2.0f, 2.0f));
 static_assert(false == collision_box_box(1.0f, 2.0f, 3.0f, 4.0f,     4.5f, 7.5f, 1.4f, 1.4f));
