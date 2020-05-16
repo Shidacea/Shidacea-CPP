e470f9e1ccffd4b3377dda4cb1b397f1eae2fa7a
Philipp Schulz <hadeweka@outlook.de>
2020-05-16 17:51:20 +0200

    Changed build config to circumvent an mruby error

--- mruby_build_config.rb
+++ mruby_build_config.rb
@@ -2,6 +2,10 @@ MRuby::Build.new do |conf|
 
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
     toolchain :visualcpp
+
+     # The definition for ssize_t is due to a compilation error from mruby on Windows, where ssize_t is apparently not defined
+     # If this gets fixed, the following line can be removed safely
+    conf.cc.flags << '-Dssize_t=int'
   else
     toolchain :gcc
   end
@@ -11,8 +15,7 @@ MRuby::Build.new do |conf|
   conf.gem :mgem => 'json'
   conf.gem :mgem => 'dir'
 
-  # The definition for ssize_t is due to a compilation error from mruby on Windows, where ssize_t is apparently not defined
-  conf.cc.flags << '-DMRB_USE_FLOAT -DMRB_UTF8_STRING -Dssize_t=int'
+  conf.cc.flags << '-DMRB_USE_FLOAT -DMRB_UTF8_STRING'
 
   conf.build_dir = ENV["MRUBY_BUILD_DIR"] || raise("MRUBY_BUILD_DIR undefined!")
 
