db9a5a1059bdd5a4849f8f5ff56ab05cb68ab58e
Philipp Schulz <hadeweka@outlook.de>
2020-06-02 22:18:35 +0200

    Added limiter to main script

--- demo_projects/Example_Shooter/scripts/Main.rb
+++ demo_projects/Example_Shooter/scripts/Main.rb
@@ -1,4 +1,5 @@
 # The absolute main file
 # Everything starts here
 
-main_routine(ShooterTest::SceneMenu, game_class: ShooterTest::Game, title: 'Shidacea Shooter', width: 1280, height: 720)
+limiter = SDC::Limiter.new(max: 720, renders_per_second: 60, ticks_per_second: 60, gc_per_second: 60)
+main_routine(ShooterTest::SceneMenu, game_class: ShooterTest::Game, title: 'Shidacea Shooter', width: 1280, height: 720, limiter: limiter)
