23d975d1254ff8290b2620b326a3c1ee9fc5627e
Philipp Schulz <hadeweka@outlook.de>
2020-07-14 22:34:25 +0200

    Added collision shape documentation

--- engine/internal/Shape.cpp
+++ engine/internal/Shape.cpp
@@ -23,35 +23,66 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 	// Module for collision test methods
 	auto module_collider = mrb_define_module_under(mrb, ruby_module, "Collider");
 
+	// @@@ M_CLASS CollisionShape
+	// Abstract class for any shape with collision properties
 	MrbWrap::wrap_class_under<Shape>(mrb, "CollisionShape", ruby_module);
 	auto ruby_shape_class = MrbWrap::get_class_info_ptr<Shape>();
 
+	// @@@ M_CLASS CollisionShapePoint
+	// Collision shape class for a single point
 	MrbWrap::wrap_class_under<ShapePoint>(mrb, "CollisionShapePoint", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeLine>(mrb, "CollisionShapeLine", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeCircle>(mrb, "CollisionShapeCircle", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeBox>(mrb, "CollisionShapeBox", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeTriangle>(mrb, "CollisionShapeTriangle", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeQuadrangle>(mrb, "CollisionShapeQuadrangle", ruby_module, ruby_shape_class);
-	MrbWrap::wrap_class_under<ShapeEllipse>(mrb, "CollisionShapeEllipse", ruby_module, ruby_shape_class);
-
 	auto ruby_shape_point_class = MrbWrap::get_class_info_ptr<ShapePoint>();
+
+	// @@@ M_CLASS CollisionShapeLine
+	// Collision shape class for a line
+	MrbWrap::wrap_class_under<ShapeLine>(mrb, "CollisionShapeLine", ruby_module, ruby_shape_class);
 	auto ruby_shape_line_class = MrbWrap::get_class_info_ptr<ShapeLine>();
+
+	// @@@ M_CLASS CollisionShapeCircle
+	// Collision shape class for a circle
+	MrbWrap::wrap_class_under<ShapeCircle>(mrb, "CollisionShapeCircle", ruby_module, ruby_shape_class);
 	auto ruby_shape_circle_class = MrbWrap::get_class_info_ptr<ShapeCircle>();
+
+	// @@@ M_CLASS CollisionShapeBox
+	// Collision shape class for an axis-aligned rectangle
+	MrbWrap::wrap_class_under<ShapeBox>(mrb, "CollisionShapeBox", ruby_module, ruby_shape_class);
 	auto ruby_shape_box_class = MrbWrap::get_class_info_ptr<ShapeBox>();
+
+	// @@@ M_CLASS CollisionShapeTriangle
+	// Collision shape class for a triangle
+	MrbWrap::wrap_class_under<ShapeTriangle>(mrb, "CollisionShapeTriangle", ruby_module, ruby_shape_class);
 	auto ruby_shape_triangle_class = MrbWrap::get_class_info_ptr<ShapeTriangle>();
+
+	// @@@ M_CLASS CollisionShapeQuadrangle
+	// Collision shape class for a quadrangle (not implemented yet)
+	MrbWrap::wrap_class_under<ShapeQuadrangle>(mrb, "CollisionShapeQuadrangle", ruby_module, ruby_shape_class);
 	auto ruby_shape_quadrangle_class = MrbWrap::get_class_info_ptr<ShapeQuadrangle>();
+
+	// @@@ M_CLASS CollisionShapeEllipse
+	// Collision shape class for an ellipse (not implemented yet)
+	MrbWrap::wrap_class_under<ShapeEllipse>(mrb, "CollisionShapeEllipse", ruby_module, ruby_shape_class);
 	auto ruby_shape_ellipse_class = MrbWrap::get_class_info_ptr<ShapeEllipse>();
 
+	// @@@ M_IM CollisionShape initialize
+	// Default constructor for each collision shape
 	MrbWrap::wrap_constructor<Shape>(mrb);
 
+	// @@@ M_ATTR CollisionShape offset Coordinates rw
+	// Offset vector of the collision shape
 	MrbWrap::wrap_getter<Shape, &Shape::getPosition>(mrb, "offset");
 	MrbWrap::wrap_setter<Shape, MrbWrap::specify<sf::Transformable, void, const sf::Vector2f&>(&Shape::setPosition), sf::Vector2f>(mrb, "offset=");
 
+	// @@@ M_ATTR CollisionShape origin Coordinates rw
+	// Origin vector of the collision shape
 	MrbWrap::wrap_getter<Shape, &Shape::getOrigin>(mrb, "origin");
 	MrbWrap::wrap_setter<Shape, MrbWrap::specify<sf::Transformable, void, const sf::Vector2f&>(&Shape::setOrigin), sf::Vector2f>(mrb, "origin=");
 
 	//! TODO: Write standard constructors accepting the respective arguments
 
+	// @@@ M_IM CollisionShapePoint initialize offset
+	// @return [CollisionShapePoint]
+	// @param offset [Coordinates]
+	// Creates a point collision shape
 	MrbWrap::define_member_function(mrb, ruby_shape_point_class, "initialize", MRUBY_FUNC {
 
 		auto args = MrbWrap::get_converted_args<sf::Vector2f>(mrb);
@@ -66,6 +97,11 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 
 	MrbWrap::define_default_copy_init<ShapePoint>(mrb);
 
+	// @@@ M_IM CollisionShapeLine initialize offset line
+	// @return [CollisionShapeLine]
+	// @param offset [Coordinates]
+	// @param line [Coordinates]
+	// Creates a line collision shape with the given line vector
 	MrbWrap::define_member_function(mrb, ruby_shape_line_class, "initialize", MRUBY_FUNC {
 
 		auto args = MrbWrap::get_converted_args<sf::Vector2f, sf::Vector2f>(mrb);
@@ -82,9 +118,16 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 
 	MrbWrap::define_default_copy_init<ShapeLine>(mrb);
 
+	// @@@ M_ATTR CollisionShapeLine line Coordinates rw
+	// Direction vector of the line shape
 	MrbWrap::wrap_getter<ShapeLine, &ShapeLine::line>(mrb, "line");
 	MrbWrap::wrap_setter<ShapeLine, &ShapeLine::line, sf::Vector2f>(mrb, "line=");
 
+	// @@@ M_IM CollisionShapeCircle initialize offset radius
+	// @return [CollisionShapeCircle]
+	// @param offset [Coordinates]
+	// @param radius [Float]
+	// Creates a circle collision shape with the given radius
 	MrbWrap::define_member_function(mrb, ruby_shape_circle_class, "initialize", MRUBY_FUNC {
 
 		auto args = MrbWrap::get_converted_args<sf::Vector2f, float>(mrb);
@@ -101,12 +144,21 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 
 	MrbWrap::define_default_copy_init<ShapeCircle>(mrb);
 
+	// @@@ M_ATTR CollisionShapeCircle radius Float rw
+	// Radius of the circle shape
 	MrbWrap::wrap_getter<ShapeCircle, &ShapeCircle::radius>(mrb, "radius");
 	MrbWrap::wrap_setter<ShapeCircle, &ShapeCircle::radius, float>(mrb, "radius=");
 
+	// @@@ M_ATTR CollisionShapeCircle scale Float rw
+	// Scale factor of the circle shape
 	MrbWrap::wrap_getter<ShapeCircle, &ShapeCircle::getSingleArgScale>(mrb, "scale");
 	MrbWrap::wrap_setter<ShapeCircle, &ShapeCircle::setSingleArgScale, float>(mrb, "scale=");
 
+	// @@@ M_IM CollisionShapeBox initialize offset size
+	// @return [CollisionShapeBox]
+	// @param offset [Coordinates]
+	// @param size [Coordinates]
+	// Creates a box collision shape with the given size
 	MrbWrap::define_member_function(mrb, ruby_shape_box_class, "initialize", MRUBY_FUNC {
 
 		auto args = MrbWrap::get_converted_args<sf::Vector2f, sf::Vector2f>(mrb);
@@ -123,12 +175,22 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 
 	MrbWrap::define_default_copy_init<ShapeBox>(mrb);
 
+	// @@@ M_ATTR CollisionShapeBox size Coordinates rw
+	// Size vector of the box shape
 	MrbWrap::wrap_getter<ShapeBox, &ShapeBox::size>(mrb, "size");
 	MrbWrap::wrap_setter<ShapeBox, &ShapeBox::size, sf::Vector2f>(mrb, "size=");
-
+	
+	// @@@ M_ATTR CollisionShapeBox scale Coordinates rw
+	// Scale factor vector of the box shape
 	MrbWrap::wrap_getter<ShapeBox, &ShapeBox::getScale>(mrb, "scale");
 	MrbWrap::wrap_setter<ShapeBox, MrbWrap::specify<sf::Transformable, void, const sf::Vector2f&>(&ShapeBox::setScale), sf::Vector2f>(mrb, "scale=");
 
+	// @@@ M_IM CollisionShapeTriangle initialize offset side_1 side_2
+	// @return [CollisionShapeTriangle]
+	// @param offset [Coordinates]
+	// @param side_1 [Coordinates]
+	// @param side_2 [Coordinates]
+	// Creates a triangle collision shape with the given side vectors
 	MrbWrap::define_member_function(mrb, ruby_shape_triangle_class, "initialize", MRUBY_FUNC {
 
 		auto args = MrbWrap::get_converted_args<sf::Vector2f, sf::Vector2f, sf::Vector2f>(mrb);
@@ -147,12 +209,18 @@ void setup_ruby_collider(mrb_state* mrb, RClass* ruby_module) {
 
 	MrbWrap::define_default_copy_init<ShapeTriangle>(mrb);
 
+	// @@@ M_ATTR CollisionShapeTriangle side_1 Coordinates rw
+	// First side vector of the triangle shape
 	MrbWrap::wrap_getter<ShapeTriangle, &ShapeTriangle::side_1>(mrb, "side_1");
 	MrbWrap::wrap_setter<ShapeTriangle, &ShapeTriangle::side_1, sf::Vector2f>(mrb, "side_1=");
 
+	// @@@ M_ATTR CollisionShapeTriangle side_2 Coordinates rw
+	// Second side vector of the triangle shape
 	MrbWrap::wrap_getter<ShapeTriangle, &ShapeTriangle::side_2>(mrb, "side_2");
 	MrbWrap::wrap_setter<ShapeTriangle, &ShapeTriangle::side_2, sf::Vector2f>(mrb, "side_2=");
 
+	// @@@ M_ATTR CollisionShapeTriangle scale Coordinates rw
+	// Scale factor vector of the triangle shape
 	MrbWrap::wrap_getter<ShapeTriangle, &ShapeTriangle::getScale>(mrb, "scale");
 	MrbWrap::wrap_setter<ShapeTriangle, MrbWrap::specify<sf::Transformable, void, const sf::Vector2f&>(&ShapeTriangle::setScale), sf::Vector2f>(mrb, "scale=");
 
